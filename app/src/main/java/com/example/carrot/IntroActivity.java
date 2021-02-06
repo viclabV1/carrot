8fd22d990d0b3da7bb1cb3cb23de752d101fd5a4
viclabV1 <vplabrie@gmail.com>
2021-02-06 01:37:41 -0600

    Layout templates, intro activity
    Made the layouts for some of the activities. Made activity class for intro, which consists of a single button action.

---
+++ app/src/main/java/com/example/carrot/IntroActivity.java
@@ -0,0 +1,22 @@
+package com.example.carrot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.introduction);
+    }
+
+    //called on button press after first time opening app
+    public void firstOpen(View view){
+        Intent intent = new Intent(this, Allowance.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/carrot/RewardActivity.java
@@ -0,0 +1,14 @@
+package com.example.carrot;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RewardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.allowance);
+    }
+}
