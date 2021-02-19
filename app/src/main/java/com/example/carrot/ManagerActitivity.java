950430158970abdd50bdae17f6a0203df6945e12
viclabV1 <vplabrie@gmail.com>
2021-02-19 16:52:49 -0600

    logo, changed color scheme,etc

--- app/src/main/java/com/example/carrot/ManagerActitivity.java
+++ app/src/main/java/com/example/carrot/ManagerActitivity.java
@@ -4,12 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class ManagerActitivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+
+
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         if(firstOpen){
             openIntro();
         }
